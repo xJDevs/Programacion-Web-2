c10436ec860c01ce6d6503b539638fdb19fb75c4
Johelagr29 <johel.agr.20629@gmail.com>
2026-06-21 16:17:31 -0600

    exceptions: se crea paquete con excepciones para 404 y 403

---
+++ Laboratorio_5/JustDoItCloudRest/src/main/java/teccr/justdoitcloud/exception/TaskForbiddenException.java
@@ -0,0 +1,7 @@
+package teccr.justdoitcloud.exception;
+
+public class TaskForbiddenException extends RuntimeException {
+    public TaskForbiddenException(String message) {
+        super(message);
+    }
+}
